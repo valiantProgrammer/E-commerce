e92a2de743ffbceeeb86f564c226c6d794cbdd21
valiantProgrammer <rupayandey134@gmail.com>
2026-01-16 16:14:43 +0530

    Update README with screenshots and cleanup project structure

--- app/about/page.js
+++
@@ -1,9 +0,0 @@
-import AboutPageContent from '@/app/components/AboutPageContent';
-
-export default function AboutPage() {
-  return (
-    <main>
-      <AboutPageContent />
-    </main>
-  );
-}
--- app/deals/page.js
+++
@@ -1,9 +0,0 @@
-import DealsPageContent from '@/app/components/DealsPageContent';
- 
-export default function DealsPage() {
-    return (
-        <main>
-            <DealsPageContent />
-        </main>
-    );
-}
